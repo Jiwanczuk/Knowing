e4f26ec5524a50511e36a1f570ef90521eb73600
Jiwanczuk <103602175+Jiwanczuk@users.noreply.github.com>
2023-02-21 09:57:44 -0300

    Update sidebar.py

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -27,13 +27,9 @@ def sidebar():
         st.markdown("---")
         st.markdown("# About")
         st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
+            "📖Margulan AI allows you to ask questions about your "
             "documents and get accurate answers with instant citations. "
-        )
-        st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "
-            "with your feedback and suggestions💡"
+ 
+    
         )
         st.markdown("---")
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
